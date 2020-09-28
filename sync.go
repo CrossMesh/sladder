2a878b1e86e5b10dc708caaafb842b6eab904584
Chad Mai <Sunmxt@126.com>
2020-09-28 19:28:33 +0800

    resolveNodeNameFromProtobuf() should get real txn.

--- sync.go
+++ sync.go
@@ -28,11 +28,31 @@ func (c *Cluster) resolveNodeNameFromProtobuf(entries []*proto.Node_KeyValue) ([
 	}
 
 	watchEntries := make([]*KeyValue, 0, len(watchKeySet))
+	actualEntry := KeyValue{}
 	for _, kv := range watchKeySet {
 		if kv == nil {
 			continue
 		}
-		watchEntries = append(watchEntries, kv)
+		validator, hasValidator := c.validators[kv.Key]
+		if !hasValidator || validator == nil {
+			continue
+		}
+		actualEntry.Key, actualEntry.Value = kv.Key, kv.Value
+		rtx, err := validator.Txn(actualEntry)
+		if err != nil {
+			return nil, err
+		}
+		for {
+			wrapper, wrapped := rtx.(KVTransactionWrapper)
+			if wrapped && wrapper != nil {
+				rtx = wrapper.KVTransaction()
+				continue
+			}
+			break
+		}
+		watchEntries = append(watchEntries, &KeyValue{
+			Key: kv.Key, Value: rtx.Before(),
+		})
 	}
 
 	names, err := c.resolver.Resolve(watchEntries...)
